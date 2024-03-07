0312b5be9e8c064a4482b03d692a3b0f20bfb970
Zakiaatot <384637134@qq.com>
2024-03-07 19:46:30 +0800

    update DrawInit Api

--- README.md
+++ README.md
@@ -40,7 +40,7 @@ static BOOL MouseMiddleButtonUp(); // 鼠标滚轮弹起
 static BOOL MouseMoveRelative(IN LONG dx, IN LONG dy); // 鼠标相对移动
 static BOOL MouseMoveAbsolute(IN LONG dx, IN LONG dy); // 鼠标绝对移动
 // Draw
-static BOOL DrawInit(IN DRAW_LOOP drawLoop, IN INT fontSize = 16); // 初始化绘制
+static BOOL DrawInit(IN DRAW_LOOP drawLoop, IN LONG width, IN LONG height, IN INT fontSize = 16);  // 初始化绘制
 static BOOL DrawDestroy(); // 结束绘制
 static BOOL DrawFps(); // 绘制FPS
 static BOOL DrawText(IN LONG x, IN LONG y, IN LPCWSTR str, IN COLORREF color, IN INT fontSize = 16); // 绘制文本
--- ZfDriver/ZfDriver.cpp
+++ ZfDriver/ZfDriver.cpp
@@ -297,7 +297,7 @@ BOOL ZfDriver::MouseMoveAbsolute(IN LONG dx, IN LONG dy)
 	return TRUE;
 }
 
-BOOL ZfDriver::DrawInit(IN DRAW_LOOP drawLoop, IN INT fontSize)
+BOOL ZfDriver::DrawInit(IN DRAW_LOOP drawLoop, IN LONG width, IN LONG height, IN INT fontSize)
 {
 	if (gIsZfDriverInstalled == FALSE)
 		return FALSE;
@@ -306,8 +306,8 @@ BOOL ZfDriver::DrawInit(IN DRAW_LOOP drawLoop, IN INT fontSize)
 		delete gPGDIObject;
 		gPGDIObject = NULL;
 	}
-	INT width = GetSystemMetrics(SM_CXSCREEN);
-	INT height = GetSystemMetrics(SM_CYSCREEN);
+	/*INT width = GetSystemMetrics(SM_CXSCREEN);
+	INT height = GetSystemMetrics(SM_CYSCREEN);*/
 	gPGDIObject = new GDI(width, height, drawLoop, fontSize);
 	HWND hwnd = gPGDIObject->GetHwnd();
 	if (hwnd)
